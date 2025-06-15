5072914679a26221d906ddb7d10d846522a7e072
tressels <kleebrang.w@gmail.com>
2025-06-15 14:00:12 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-<React code from Canvas will be inserted here>
+import React from "react";
+import MicrowaveSampleForm from "./MicrowaveSampleForm";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-6">
+      <MicrowaveSampleForm />
+    </div>
+  );
+}
+
+export default App;
